e978a8f2d91c83204ddcc850f2e9621ef3889d42
Philip Harrington <71712.2415@compuserve.com>
2026-03-09 15:59:02 -0600

    tweak to previous

--- src/main/java/frc/robot/SwerveModule.java
+++ src/main/java/frc/robot/SwerveModule.java
@@ -19,7 +19,6 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.SwvModConst;
 import frc.robot.Constants.DrvConst.Setup;
 /**Represents a wheel and its necessary motors, sensors, and information. */
