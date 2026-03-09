33a629517d6acfca4c2bc7138e7a5831c985a55b
ForgeBuilder <elliotkarlson@gmail.com>
2026-03-09 17:02:33 -0500

    Imports to the drive class and identify in comments what code I need to write for path planner to work

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import com.studica.frc.AHRS;
 
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
@@ -11,6 +14,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DrvConst;
@@ -50,7 +54,53 @@ public class Drivetrain extends SubsystemBase {
     // wait till gyro is ready
     while(gyro.isCalibrating());
     zeroYaw();
-  }
+
+    // Load the RobotConfig from the GUI settings. You should probably
+    // store this in your Constants file
+    //TODO: figure out how to integrate path planner starting here
+    RobotConfig config;
+    try{
+      config = RobotConfig.fromGUISettings();
+    } catch (Exception e) {
+      // Handle exception as needed
+      e.printStackTrace();
+    }
+
+    //TODO: create methods bellow then you can comment this in and continue to resolve.
+    
+    //driveRobotRelative
+    //getPose
+    //resetPose
+    //getRobotRelativeSpeeds
+
+    //https://pathplanner.dev/pplib-getting-started.html#install-pathplannerlib - This is where 
+
+    // Configure AutoBuilder last
+  //   AutoBuilder.configure(
+  //           this::getPose, // Robot pose supplier
+  //           this::resetPose, // Method to reset odometry (will be called if your auto has a starting pose)
+  //           this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+  //           (speeds, feedforwards) -> driveRobotRelative(speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module feedforwards
+  //           new PPHolonomicDriveController( // PPHolonomicController is the built in path following controller for holonomic drive trains
+  //                   new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+  //                   new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+  //           ),
+  //           config, // The robot configuration
+  //           () -> {
+  //             // Boolean supplier that controls when the path will be mirrored for the red alliance
+  //             // This will flip the path being followed to the red side of the field.
+  //             // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+  //             var alliance = DriverStation.getAlliance();
+  //             if (alliance.isPresent()) {
+  //               return alliance.get() == DriverStation.Alliance.Red;
+  //             }
+  //             return false;
+  //           },
+  //           this // Reference to this subsystem to set requirements
+  //     );
+    
+  // }
 
   // Whichever way we are facing is now considered forward
   void zeroYaw() {
