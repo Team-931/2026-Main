3c8aae73539ed3fe1c1fe2296920bbc7dcd7465e
ForgeBuilder <elliotkarlson@gmail.com>
2026-03-15 11:00:33 -0500

    Smartshooter
    doesn't shoot untill spun up. has an odd tendancy to DC the robot but I think that's a battery issue.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -183,14 +183,20 @@ TrajectoryWrap trajectoryWrap = new TrajectoryWrap();
     
 //button board not working at all?
     if(opController.getRawButtonPressed(ButtonBoard.Shoot)) {
-      feeder.run(true,false);
-      shooter.setTransfer(true,false);
-      shooter.shoot_with_velocity(43.3); //This speed juggles the fuel, doesn't shoot em very far.
+      shooter.shoot_with_velocity(50);
+    }
+    if(opController.getRawButton(ButtonBoard.Shoot)){
+      //43.3 - Juggling speed. Doesn't shoot far.
+      //60 - untested
+      if (shooter.get_shooter_ready(3)){
+        feeder.run(true,false);
+        shooter.setTransfer(true,false);
+      }
     }
     if(opController.getRawButtonReleased(ButtonBoard.Shoot)) {
       feeder.run(false,false); 
       shooter.setTransfer(false,false);
-      shooter.shoot_with_velocity(0);
+      shooter.shoot_with_velocity(0); //we don't really care what it returns
     }
     if(opController.getRawButtonPressed(ButtonBoard.FeederReverse)){
       shooter.setTransfer(true,true);
